84053861c835a81d153a68c1e0a83f515b455663
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-18 16:52:43 +0000

    Edited code attributes for task 3.

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """
 Module for BaseModel class
 """
-import datetime
+from datetime import datetime
 import uuid
 import inspect
 
@@ -17,20 +17,20 @@ class BaseModel:
         Constructor of class BaseModel instance
         """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.now(tz=None)
+        self.updated_at = datetime.now(tz=None)
 
     def __str__(self):
         """
         String representation of the instance created
         """
-        return "[{}] ({}) {}".format(type(self), self.id, self.dict)
+        return "[{}] ({}) {}".format(type(self), self.id, self.__dict__)
 
     def save(self):
         """
         Updates 'updated_at' with current datetime
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.now(tz=None)
 
     def to_dict(self):
         """
@@ -38,12 +38,12 @@ class BaseModel:
         Filters data that starts with underscores, methods, and functions.
         """
         my_dict = {}
-        my_dict['__class__'] = self.__name__
+        my_dict['__class__'] = type(self)
         self.created_at.isoformat()
         self.updated_at.isoformat()
         for i in inspect.getmembers(self):
             if not i[0].startswith("_"):
-                if not inspect.ismethod(i[1] and not\
+                if not inspect.ismethod(i[1]) and not\
                        inspect.isfunction(i[1]):
                     my_dict[i[0]] = i[1]
         return my_dict
