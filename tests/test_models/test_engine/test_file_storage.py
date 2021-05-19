47e99bd47c3969fabd96d1294d1e66c5abc4445a
kyle-gross <kyg555@outlook.com>
2021-05-19 21:23:17 +0000

    Add save() test.

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,6 +4,7 @@ Contains tests for FileStorge class
 """
 import unittest
 import os
+import json
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
@@ -13,11 +14,21 @@ class TestFileStorage(unittest.TestCase):
     Tests for FileStorage class
     """
 
-    def test_file_path(self):
+    def test_others(self):
         file_path = "file.json"
         if not os.path.exists(file_path):
             model = BaseModel()
         self.assertTrue(os.path.exists(file_path))
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        model = BaseModel()
+        model.save()
+        with open(file_path, "r") as f:
+            original = f.read()
+        model.save()
+        with open(file_path, "r") as f:
+            new = f.read()
+        self.assertNotEqual(original, new)
 
     def test_obj_dict(self):
         model = BaseModel()
