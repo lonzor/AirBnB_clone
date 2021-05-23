03153b4bce62aa3494fdf7cc20c7451a4db5449c
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-05-23 00:20:14 +0000

    Added new classes to FileStorage and console.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,11 @@ Module for class FileStorage
 """
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 import json
 import os
 
@@ -14,7 +19,9 @@ class FileStorage:
     Deserializes JSON file to instances
     """
 
-    cls_dict = {"BaseModel": BaseModel, "User": User}
+    cls_dict = {"BaseModel": BaseModel, "User": User, "Place": Place,
+                "State": State, "City": City, "Amenity": Amenity,
+                "Review": Review}
     __file_path = 'file.json'
     __objects = {}
 
